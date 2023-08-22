f8ed65455c57c53fa5670b965f890d22f92eacae
David Hockley <david.hockley@gmail.com>
2023-08-22 06:09:22 +0200

    clean up

--- app/server.go
+++ app/server.go
@@ -10,8 +10,7 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
+
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 	 	fmt.Println("Failed to bind to port 6379")
@@ -30,7 +29,9 @@ func main() {
 	buf := make([]byte, 1024);
 
 	for i := 0; i < 10; i++ {
+
 		size, readerr  := conn.Read(buf);
+
 		fmt.Printf("Read : %d\n", size);
 		if readerr != nil {
 			fmt.Println("Error reading: ", readerr.Error())
