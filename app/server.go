eeaa89ca8d17489819dbe8ff259686c97be0fa00
David Hockley <david.hockley@gmail.com>
2023-08-22 05:53:00 +0200

    let's keep moving

--- app/server.go
+++ app/server.go
@@ -19,12 +19,29 @@ func main() {
 	}
 
 	conn, err := l.Accept()
+
 	if err != nil {
 	 	fmt.Println("Error accepting connection: ", err.Error())
 	 	os.Exit(1)
 	}
 
-	conn.Write([]byte("+PONG\r\n"))
-
 	fmt.Printf("Listening on host: %s, port: %s\n", "0.0.0.0", "6379")
+
+	buf := make([]byte, 1024);
+
+	conn.Write([]byte("+PONG\r\n"));
+
+	for i := 0; i < 10; i++ {
+		size, readerr  := conn.Read(buf);
+		fmt.Printf("Read : %d\n", size);
+		if readerr != nil {
+			fmt.Println("Error reading: ", readerr.Error())
+			break;
+		}
+
+		conn.Write([]byte("+PONG\r\n"));
+
+	}
+
+
 }
