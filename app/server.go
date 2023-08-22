266586572041e56fce47641be930e45f129d7fcb
David Hockley <david.hockley@gmail.com>
2023-08-22 06:18:33 +0200

    test multiple clients

--- app/server.go
+++ app/server.go
@@ -6,29 +6,14 @@ import (
 	"os"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
 
-
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-	 	fmt.Println("Failed to bind to port 6379")
-	 	os.Exit(1)
-	}
-
-	conn, err := l.Accept()
-
-	if err != nil {
-	 	fmt.Println("Error accepting connection: ", err.Error())
-	 	os.Exit(1)
-	}
+func handle(conn net.Conn) {
 
 	fmt.Printf("Listening on host: %s, port: %s\n", "0.0.0.0", "6379")
 
 	buf := make([]byte, 1024);
 
-	for i := 0; i < 10; i++ {
+	for true {
 
 		size, readerr  := conn.Read(buf);
 
@@ -41,6 +26,32 @@ func main() {
 		conn.Write([]byte("+PONG\r\n"));
 
 	}
+}
+
+
+func main() {
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
+
+
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+	 	fmt.Println("Failed to bind to port 6379")
+	 	os.Exit(1)
+	}
+
+
+	for true {
+		conn, err := l.Accept()
+
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handle(conn);
+	}
+
 
 
 }
